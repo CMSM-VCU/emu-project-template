6aa11c3d05cd1c7da98f1eb62282604f6db12b09
Riley Hall <riley.hall.va@gmail.com>
2021-09-23 16:25:48 -0400

    Initial commit

---
+++ new_job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Input file name without extension is first argument
+
+function yes_or_no() {
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) echo "Aborted" ; return  1 ;;
+        esac
+    done
+}
+
+if [ $# -eq 0 ] ; then
+    echo "No argument given"
+else
+    for job in "$@"
+    do
+        yes_or_no "Submit $job for debug?" && {
+        cp job_base.slurm ./slurm_scripts/job/job_$job.slurm
+
+        cd ./slurm_scripts/job
+        sed -i -e 's/'"TITLE"'/'"$job"'/' job_$job.slurm
+        sbatch job_$job.slurm
+
+        cd ../..
+        }
+    done
+fi
---
+++ postprocess.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ] ; then
+    echo "No argument given"
+else
+    for job in "$@"
+    do
+        mkdir ./outputs/$job
+        cd ./$job/plotfiles
+        python ../../emu_to_h5.py
+
+        cd ..
+        cp -t ../outputs/$job ./emu.out.0.0 ./*.in ./emu.his ./plotfiles/simulation.h5
+
+        cd ..
+    done
+fi
