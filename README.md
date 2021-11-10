86a1e2322d190d138bc1ce507f3c885b002db199
Riley Hall <riley.hall.va@gmail.com>
2021-11-10 13:22:37 -0500

    Add some clarifications and generalization

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # emu-project-template
+
+***NOTE: The job_base.\* files require configuration for your specific use case. This will not work out of the box.***
--- postprocess.sh
+++ postprocess.sh
@@ -6,13 +6,14 @@ if [ $# -eq 0 ] ; then
 else
     for job in "$@"
     do
-        mkdir ./outputs/$job
-        cd ./$job/plotfiles
-        python ../../emu_to_h5.py
+        # This is just an example:
+        # mkdir ./outputs/$job
+        # cd ./$job/plotfiles
+        # python ../../emu_to_h5.py
 
-        cd ..
-        cp -t ../outputs/$job ./emu.out.0.0 ./*.in ./emu.his ./plotfiles/simulation.h5
+        # cd ..
+        # cp -t ../outputs/$job ./emu.out.0.0 ./*.in ./emu.his ./plotfiles/simulation.h5
 
-        cd ..
+        # cd ..
     done
 fi
