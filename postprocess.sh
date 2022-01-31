3918d53845c6c6201e36ec92d05ad745bfc4a29a
Riley Hall <riley.hall.va@gmail.com>
2022-01-31 17:04:35 -0500

    Revise postprocess script to be minimally functional

--- postprocess.sh
+++ postprocess.sh
@@ -7,13 +7,16 @@ else
     for job in "$@"
     do
         # This is just an example:
-        # mkdir ./outputs/$job
-        # cd ./$job/plotfiles
+        mkdir ./outputs/$job
+        cd ./$job/
+        # Plotfile generation depends on your own setup.
+        # cd ./plotfiles
         # python ../../emu_to_h5.py
-
         # cd ..
-        # cp -t ../outputs/$job ./emu.out.0.0 ./*.in ./emu.his ./plotfiles/simulation.h5
 
-        # cd ..
+        cp -t ../outputs/$job ./emu.out.0.0 ./emu.his ./*.in
+        # cp -t ../outputs/$job ./plotfiles/simulation.h5
+
+        cd ..
     done
 fi
